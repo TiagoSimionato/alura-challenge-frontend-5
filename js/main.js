360210dfe15725f9506519256aff67d10d978f0d
tiago-simionato <tiago.simionato5@gmail.com>
2022-09-28 14:08:08 -0300

    fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const consoleScroller = document.querySelector('#console-scroller');
+const searchElement = document.querySelector('#search-box');
 
 consoleScroller.addEventListener('click', () => {
     document.querySelector('#console-section').scrollIntoView();
+});
+
+searchElement.addEventListener('change', () => {
+    window.alert('Isso não faz nada!');
 });
